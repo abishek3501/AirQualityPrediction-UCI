21b82447fd1e2733b68e48a24fabbca82e9f7ba6
abishek3501 <abisheksjns1997@gmail.com>
2018-07-25 21:57:48 +0530

    Update airqualityuci.py
    Added a 3-layer Deep Neural Network regressor model. Input features are made polynomial and the hidden layers had Batch-normalization and ReLu activation function. The final layer was a simple linear regression layer with no activation function.

--- airqualityuci.py
+++ airqualityuci.py
@@ -5,12 +5,29 @@ Created on Wed Jul 18 08:51:47 2018
 @author: Narayanan Abishek
 """
 
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 18 08:51:47 2018
+
+@author: Narayanan Abishek
+"""
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from sklearn.metrics import explained_variance_score
- 
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers import Activation
+from keras.layers.normalization import BatchNormalization, regularizers
+from keras.utils import np_utils
+from sklearn import metrics 
+from sklearn.cross_validation import cross_val_score, cross_val_predict
+from sklearn.model_selection import StratifiedKFold, KFold
+
 lamda = 0.01
 
 #original dataset
@@ -79,7 +96,6 @@ def linear_regression_simple(train_X,train_Y,val_X,val_Y,test_X,test_Y,w,b,learn
         test_predictions,test_cost = calc(test_X,test_Y,w,b)
         test_Y_pred = test_predictions.eval()
         
-    from sklearn import metrics 
    
     print("\nTraining set accuracy: \n")
     print('Mean Absolute Error:', metrics.mean_absolute_error(train_Y,pred))  
@@ -102,6 +118,54 @@ def linear_regression_simple(train_X,train_Y,val_X,val_Y,test_X,test_Y,w,b,learn
     print("\nActual Train Value: ", train_Y[82], "Predicted Train value: ", pred[82])
     print("\nActual Validation Value: ", val_Y[834], "Predicted Validation value: ", val_Y_pred[834])
     print("\nActual Test Value: ", test_Y[948], "Predicted Test value: ", test_Y_pred[948])
+    
+    return test_Y_pred
+
+def nn_regression(final_train_X,train_y,final_val_X,val_y,final_test_X,test_y):
+    
+    model = Sequential()
+    model.add(Dense(90, input_dim = 90,kernel_initializer='normal',kernel_regularizer = regularizers.l2(0.00025)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dense(40,kernel_initializer='normal',kernel_regularizer = regularizers.l2(0.00025)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dense(40,kernel_initializer='normal',kernel_regularizer = regularizers.l2(0.00025)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dense(40,kernel_initializer='normal',kernel_regularizer = regularizers.l2(0.00025)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dense(1,kernel_initializer = 'normal',kernel_regularizer = regularizers.l2(0.00025)))
+    model.compile(loss = 'mean_squared_error', optimizer = 'adam')
+    
+    trained_model = model.fit(final_train_X, train_y, epochs = 200,validation_data=(final_val_X,val_y))
+    
+    y_pred = model.predict(final_test_X)
+    
+    print("\nTesting set accuracy: \n")
+     
+    print('Mean Absolute Error:', metrics.mean_absolute_error(test_y, y_pred))  
+    print('Mean Squared Error:', metrics.mean_squared_error(test_y, y_pred))  
+    print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(test_y, y_pred)))
+
+    print("\nActual Test Value: ", test_y[500], "Predicted Test value: ", y_pred[500])
+    
+
+    plt.plot(trained_model.history['loss'], color = 'red', label = 'Train data loss')
+    plt.plot(trained_model.history['val_loss'], color = 'blue', label = 'Validation data loss')
+    plt.title('Loss')
+    plt.legend()
+    plt.show()
+
+    return y_pred
+
+def comparison(test_y,lin_reg_pred_y,nn_reg_pred_y):
+    
+    plt.plot(np.abs(test_y - lin_reg_pred_y), color = 'red', label = 'Difference between Test data and linear regression predicted values')
+    plt.plot(np.abs(test_y - nn_reg_pred_y), color = 'blue', label = 'Difference between Test data and DNN regressor predicted values')
+    plt.legend()
+    plt.show()
     
 def model():
     
@@ -126,7 +190,7 @@ def model():
         
     train_x, val_x, train_y,val_y = train_test_split(X, Y, test_size=0.2, random_state=0) 
     
-    train_x, test_x, train_y,test_y = train_test_split(train_x, train_y, test_size=0.2, random_state=0) 
+    train_x, test_x, train_y,test_y = train_test_split(train_x,train_y, test_size=0.2, random_state=0) 
  
     train_X = np.delete(train_x, 0, axis=1)
     final_train_X = feature_scaling(train_X)
@@ -147,6 +211,9 @@ def model():
     print("Shape of test-X-data:",final_test_X.shape)
     print("Shape of test-Y-data:",test_y.shape)
     
-    linear_regression_simple(final_train_X,train_y,final_val_X,val_y,final_test_X,test_y,w,b)
+    lin_reg_pred_y = linear_regression_simple(final_train_X,train_y,final_val_X,val_y,final_test_X,test_y,w,b)
+    nn_reg_pred_y = nn_regression(final_train_X,train_y,final_val_X,val_y,final_test_X,test_y)
     
+    comparison(test_y,lin_reg_pred_y,nn_reg_pred_y)
+
 model()
